223d0e67973291af3a45188f491b8b6364e04732
Laxariust <marcin.brzywczy@edu.uekat.pl>
2018-12-14 16:48:06 +0100

    Added main menu scene

---
+++ Assets/Scripts/MainMenuScripts.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnContinueButtonClick()
+    {
+        //TODO
+    }
+
+    public void OnStartButtonClick()
+    {
+        //TODO
+    }
+
+    public void OnLoadButtonClick()
+    {
+        //TODO
+    }
+
+    public void OnSettingsButtonClick()
+    {
+        //TODO
+    }
+
+    public void OnExitButtonClick()
+    {
+        Application.Quit();
+    }
+}
